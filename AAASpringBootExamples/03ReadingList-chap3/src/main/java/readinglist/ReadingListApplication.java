2061172819be3ee558d393ce23bab9925c52f318
franck-benault <franck-benault@orange.fr>
2017-07-18 23:17:18 +0200

    complete chapter3

--- AAASpringBootExamples/03ReadingList-chap3/src/main/java/readinglist/ReadingListApplication.java
+++ AAASpringBootExamples/03ReadingList-chap3/src/main/java/readinglist/ReadingListApplication.java
@@ -1,13 +1,14 @@
-package readingList;
+package readinglist;
+
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class ReadingListApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
@@ -16,7 +17,13 @@ public class ReadingListApplication extends WebMvcConfigurerAdapter {
     
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-      registry.addRedirectViewController("/", "/readingList");
+      registry.addViewController("/login").setViewName("login");
+    }
+    
+    @Override
+    public void addArgumentResolvers(
+        List<HandlerMethodArgumentResolver> argumentResolvers) {
+      argumentResolvers.add(new ReaderHandlerMethodArgumentResolver());
     }
     
 }
